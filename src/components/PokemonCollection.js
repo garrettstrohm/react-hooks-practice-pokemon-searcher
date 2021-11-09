f83334398bde2160862100bbee0fa8f5901e50a7
Garrett <garres4@vt.edu>
2021-11-09 16:36:43 -0700

    commit tree 89db8bafb587e7574b511b07fa85d00f8485548c

--- src/components/PokemonCollection.js
+++ src/components/PokemonCollection.js
@@ -2,10 +2,12 @@ import React from "react";
 import PokemonCard from "./PokemonCard";
 import { Card } from "semantic-ui-react";
 
-function PokemonCollection() {
+function PokemonCollection({ pokemons, isLoaded }) {
+  const pokemonCards = pokemons.map(pokemon => <PokemonCard key={pokemon.id} name={pokemon.name} hp={pokemon.hp} frontSprite={pokemon.sprites.front} backSprite={pokemon.sprites.back}/>)
+
   return (
     <Card.Group itemsPerRow={6}>
-      <h1>Hello From Pokemon Collection</h1>
+      {isLoaded ? pokemonCards : <h1>Loading...</h1>}
     </Card.Group>
   );
 }
--- src/components/PokemonPage.js
+++ src/components/PokemonPage.js
@@ -1,19 +1,58 @@
-import React from "react";
+import React,{useState, useEffect} from "react";
 import PokemonCollection from "./PokemonCollection";
 import PokemonForm from "./PokemonForm";
 import Search from "./Search";
 import { Container } from "semantic-ui-react";
 
 function PokemonPage() {
+  const[search, setSearch] = useState("")
+  const [pokemons, setPokemons] = useState([])
+  const [isLoaded, setIsLoaded] = useState(false)
+  
+  useEffect(() => {
+    fetch('http://localhost:3001/pokemon')
+    .then(r => r.json())
+    .then(pokemons => {
+      setPokemons(pokemons)
+      setIsLoaded(isLoaded => !isLoaded)
+    })
+  }, [])
+  
+  function handleSubmitEvent(newPoke) {
+  
+    // refactoring pokeObj
+    const addedPoke = {
+      id: "",
+      name: newPoke.name,
+      hp: newPoke.hp,
+      sprites: {
+        front: newPoke.front,
+        back: newPoke.back
+      }
+    }
+    // updatedPoke POST Req
+    fetch(`http://localhost:3001/pokemon`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(addedPoke)
+    })
+    .then(r => r.json())
+    .then(updatedPokemon => setPokemons([...pokemons, updatedPokemon]))
+  }
+
+  const pokemonList = pokemons.filter(pokemon => pokemon.name.toLowerCase().includes(search.toLowerCase()))
+
   return (
     <Container>
       <h1>Pokemon Searcher</h1>
       <br />
-      <PokemonForm />
+      <PokemonForm handleSubmitEvent={handleSubmitEvent}/>
       <br />
-      <Search />
+      <Search search={search} setSearch={setSearch}/>
       <br />
-      <PokemonCollection />
+      <PokemonCollection pokemons={pokemonList} isLoaded={isLoaded}/>
     </Container>
   );
 }
