1b47ea14367329605a31a021ae016a684d4cf5a5
Garrett <garres4@vt.edu>
2021-11-09 16:36:54 -0700

    im done

--- src/components/PokemonCard.js
+++ src/components/PokemonCard.js
@@ -1,20 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import { Card } from "semantic-ui-react";
 
-function PokemonCard() {
+function PokemonCard({ name, hp, frontSprite, backSprite }) {
+const [shownImage, setShownImage] = useState(frontSprite)
+
+function handleClick(){
+  if(shownImage === frontSprite){
+    setShownImage(backSprite)
+  } else if(shownImage === backSprite){
+    setShownImage(frontSprite)
+  }
+}
+
   return (
-    <Card>
+    <Card id={shownImage} onClick={handleClick}>
       <div>
         <div className="image">
-          <img alt="oh no!" />
+          <img src={shownImage} alt="oh no!" />
         </div>
         <div className="content">
-          <div className="header">POKEMON NAME HERE</div>
+          <div className="header">{name}</div>
         </div>
         <div className="extra content">
           <span>
             <i className="icon heartbeat red" />
-            POKEMON HP HERE hp
+            {hp}
           </span>
         </div>
       </div>
--- src/components/PokemonForm.js
+++ src/components/PokemonForm.js
@@ -1,29 +1,57 @@
-import React from "react";
+
+import React,{useState} from "react";
 import { Form } from "semantic-ui-react";
 
-function PokemonForm() {
+function PokemonForm({handleSubmitEvent}) {
+  
+const [pokeData, setPokeData] = useState({
+  id: "",
+  name: "",
+  hp: "",
+  front: "",
+  back: ""
+})
+
+function handleSubmitClick(e){
+  e.preventDefault();
+  handleSubmitEvent(pokeData)
+  setPokeData({
+    id: "",
+    name: "",
+    hp: "",
+    front: "",
+    back: ""
+  })
+}
+
+function handleChange(e){
+  setPokeData({...pokeData, [e.target.name]: e.target.value})
+}
+
   return (
     <div>
       <h3>Add a Pokemon!</h3>
       <Form
-        onSubmit={() => {
-          console.log("submitting form...");
-        }}
+        onSubmit={handleSubmitClick}
       >
         <Form.Group widths="equal">
-          <Form.Input fluid label="Name" placeholder="Name" name="name" />
-          <Form.Input fluid label="hp" placeholder="hp" name="hp" />
+          <Form.Input fluid label="Name" placeholder="Name" name="name" value={pokeData.name} onChange={handleChange}/>
+          <Form.Input fluid label="hp" placeholder="hp" name="hp" value={pokeData.hp} onChange={handleChange}/>
           <Form.Input
             fluid
             label="Front Image URL"
             placeholder="url"
-            name="frontUrl"
+            name="front"
+            value={pokeData.front}
+            onChange={handleChange}
           />
           <Form.Input
             fluid
             label="Back Image URL"
             placeholder="url"
-            name="backUrl"
+            name="back"
+            value={pokeData.back}
+            onChange={handleChange}
           />
         </Form.Group>
         <Form.Button>Submit</Form.Button>
